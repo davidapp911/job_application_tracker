cb04cea22eea928136a5c0249340c53a04db1e64
davidapp911 <davidapp91@gmail.com>
2026-04-09 18:47:00 -0500

    Added MissingUpdateFields, MissingSearchCriteria and EntryNotfound exceptions

--- src/exceptions.py
+++ src/exceptions.py
@@ -1,4 +1,11 @@
-__all__ = ["EntryException", "MissingCompany", "MissingJobTitle"]
+__all__ = [
+    "EntryException",
+    "MissingCompany",
+    "MissingJobTitle",
+    "MissingUpdateFields",
+    "MissingSearchCriteria",
+    "EntryNotFound",
+]
 
 
 class EntryException(Exception):
@@ -9,9 +16,24 @@ class EntryException(Exception):
 
 class MissingCompany(EntryException):
     def __init__(self) -> None:
-        super().__init__("Company name is missing")
+        super().__init__("Company name is required.")
 
 
 class MissingJobTitle(EntryException):
     def __init__(self) -> None:
-        super().__init__("Job title is missing")
+        super().__init__("Job title is required.")
+
+
+class MissingUpdateFields(EntryException):
+    def __init__(self) -> None:
+        super().__init__("No fields provided for update.")
+
+
+class MissingSearchCriteria(EntryException):
+    def __init__(self) -> None:
+        super().__init__("No search criteria provided.")
+
+
+class EntryNotFound(EntryException):
+    def __init__(self, value) -> None:
+        super().__init__(f"Entry with id {value} not found.")
